400f6ae15a6299bab86a5f2a65bcf10aeefec3d4
MTVirux <supersilver-dark@hotmail.com>
2019-12-17 13:29:07 +0000

    C:/Program Files/Git/shrug

--- mysqldatabasetest/main.cpp
+++ mysqldatabasetest/main.cpp
@@ -1,61 +1,40 @@
-#include <iostream>
+#include <SQLAPI.h>
+#include <stdio.h>
 
-/*
-  Include directly the different
-  headers from cppconn/ and mysql_driver.h + mysql_util.h
-  (and mysql_connection.h). This will reduce your build time!
-*/
-#include "C:/Program Files/MySQL/Connector C++ 8.0/include/jdbc/mysql_connection.h"
-
-#include <cppconn/driver.h>
-#include <cppconn/exception.h>
-#include <cppconn/resultset.h>
-#include <cppconn/statement.h>
-
-using namespace std;
-
-int main(void)
+void MySQL_ShowTables()
 {
-cout << endl;
-cout << "Running 'SELECT 'Hello World!' »
-   AS _message'..." << endl;
+	SAConnection con;
+	try
+	{
+		con.Connect("localhost@mysql", "ODBC", "", SA_MySQL_Client);
+		printf("SERVER: %s\n", (const char*)con.ServerVersionString());
+		printf("CLIENT VER: %d.%d\n\n",
+			(con.ClientVersion() >> 16),
+			(con.ClientVersion() &0x0000FFFF));
 
-try {
-  sql::Driver *driver;
-  sql::Connection *con;
-  sql::Statement *stmt;
-  sql::ResultSet *res;
+		SACommand cmd(&con);
+		//cmd.setCommandText("SHOW TABLES");
+		cmd.setCommandText("SHOW COLUMNS from user");
+		cmd.Execute();
 
-  /* Create a connection */
-  driver = get_driver_instance();
-  con = driver->connect("tcp://127.0.0.1:3306", "root", "root");
-  /* Connect to the MySQL test database */
-  con->setSchema("test");
+		while( cmd.FetchNext() )
+		{
+			for( int i = 1; i <= cmd.FieldCount(); ++i)
+			{
+				printf("%s: %s, %s\n",
+					(const char*)cmd[i].Name(),
+					(const char*)cmd[i].asBytes(),
+					(const char*)cmd[i].asString());
+			}
+			printf("\n");
+		}
+		printf("\n");
+	}
+    catch(SAException &x)
+    {
+        // print error message
+        printf("%s\n", (const char*)x.ErrText());
+    }
 
-  stmt = con->createStatement();
-  res = stmt->executeQuery("SELECT 'Hello World!' AS _message");
-  while (res->next()) {
-    cout << "\t... MySQL replies: ";
-    /* Access column data by alias or column name */
-    cout << res->getString("_message") << endl;
-    cout << "\t... MySQL says it again: ";
-    /* Access column data by numeric offset, 1 is the first column */
-    cout << res->getString(1) << endl;
-  }
-  delete res;
-  delete stmt;
-  delete con;
-
-} catch (sql::SQLException &e) {
-  cout << "# ERR: SQLException in " << __FILE__;
-  cout << "(" << __FUNCTION__ << ") on line " »
-     << __LINE__ << endl;
-  cout << "# ERR: " << e.what();
-  cout << " (MySQL error code: " << e.getErrorCode();
-  cout << ", SQLState: " << e.getSQLState() << " )" << endl;
 }
 
-cout << endl;
-
-return EXIT_SUCCESS;
-}
